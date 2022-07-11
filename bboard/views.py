b4c31596f7aac0dc38645c56248db3b93389531d
aleksander <landaralex2@gmail.com>
2022-07-11 21:26:29 +0300

    adminsite

--- bboard/views.py
+++ bboard/views.py
@@ -1,4 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+#from django.http import HttpResponse
+#from django.template import loader
+from .models import Bb
+
+"""
+def index(request):
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs': bbs}
+    return HttpResponse(template.render(context, request))
+"""
 
 def index(request):
-    return HttpResponse("Here you will see a list of advertisments")
+    bbs = Bb.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs': bbs})
+
