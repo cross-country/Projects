4c0e7e02931588ebcebbeead265a7168c1987b10
aleksander <landaralex2@gmail.com>
2022-07-21 22:19:10 +0300

    page_65

--- bboard/views.py
+++ bboard/views.py
@@ -3,9 +3,21 @@ from django.shortcuts import render
 #from django.template import loader
 from .models import Bb
 from .models import Rubric
+from django.views.generic.edit import CreateView
+from .forms import BbForm
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = '/bboard/'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
 
 def index(request):
     bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
     context = {'bbs': bbs, 'rubrics': rubrics}
     return render(request, 'bboard/index.html', context)
 
